31f52f4b3154c18f9b099c920bccdf6ad3ffddd7
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2023-04-06 16:00:27 +0200

    ContactForm component done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm';
 
 export class App extends Component {
   state = {
@@ -9,8 +10,6 @@ export class App extends Component {
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
-    name: '',
-    number: '',
     filter: '',
   };
 
@@ -24,9 +23,8 @@ export class App extends Component {
     }
   };
 
-  addContact = event => {
+  addContact = (event, name, number) => {
     event.preventDefault();
-    const { name, number } = this.state;
     const newContact = {
       name: name,
       number: number,
@@ -36,8 +34,6 @@ export class App extends Component {
     this.setState(prevState => {
       return { contacts: [...prevState.contacts, newContact] };
     });
-
-    this.setState({ name: '', number: '' });
   };
 
   render() {
@@ -45,31 +41,7 @@ export class App extends Component {
     return (
       <>
         <h1>Phonebook</h1>
-        <form>
-          <label>Name</label>
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            value={name}
-            onChange={this.onChange}
-          />
-          <label>Number</label>
-          <input
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            value={number}
-            onChange={this.onChange}
-          />
-          <button type="submit" onClick={this.addContact}>
-            Add contact
-          </button>
-        </form>
+        <ContactForm onSubmit={this.addContact} />
         <h1>Contacts</h1>
         <p>Find contacts by name</p>
         <input
---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+export default class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  onChange = event => {
+    if (event.target.name === 'name') {
+      this.setState({ name: event.target.value });
+    } else if (event.target.name === 'number') {
+      this.setState({ number: event.target.value });
+    } else if (event.target.name === 'filter') {
+      this.setState({ filter: event.target.value });
+    }
+  };
+
+  render() {
+    const { name, number } = this.state;
+    return (
+      <form>
+        <label>Name</label>
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          value={name}
+          onChange={this.onChange}
+        />
+        <label>Number</label>
+        <input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          value={number}
+          onChange={this.onChange}
+        />
+        <button
+          type="submit"
+          onClick={event => {
+            this.props.onSubmit(event, name, number);
+            this.setState({ name: '', number: '' });
+          }}
+        >
+          Add contact
+        </button>
+      </form>
+    );
+  }
+}
