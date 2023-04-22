940af10fda8249f36af6bb912be18bb0790bc1c8
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2023-04-22 18:12:46 +0200

    Added slices

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { nanoid } from 'nanoid';
+
+const contactsInitialState = [];
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: contactsInitialState,
+  reducers: {
+    addContact(state, action) {
+      state.push(action.payload);
+    },
+    prepare(contact) {
+      return {
+        payload: {
+          id: nanoid(),
+          name: contact.name,
+          number: contact.number,
+        },
+      };
+    },
+    deleteContact(state, action) {
+      state.filter(contact => {
+        return contact.id !== action.payload;
+      });
+    },
+  },
+});
+
+export const { addContact, deleteContact } = contactsSlice.actions;
+export const tasksReducer = contactsSlice.reducer;
