9db27b4909afbfe99e507e350ecfe6a1c2536195
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2023-05-21 21:04:20 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { persistor } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <PersistGate loading={null} persistor={persistor}>
         <Provider store={store}>
           <App />
